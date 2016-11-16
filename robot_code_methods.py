0c97dd47fc169b5ef2ea3ab07b8000781a146e40
maokat12 <maokat12@gmail.com>
2016-11-15 22:06:28 -0500

    Combined Lift/Identify Functions

--- robot_code_methods.py
+++ robot_code_methods.py
@@ -1,54 +1,52 @@
-from time import *
+import time
 from nxt.sensor import Light, Sound, Touch, Ultrasonic
 from nxt.motor import Motor
 
-#FUNCTION FOR BIN IDENTIFICATIION
-def bin_identify(time):
-	#use lambda function?
-		#linear function to determine where on the scale time lies
-		#you'll have to do math here
-		
-	bin_weight = lambda time: 5*time #to be changed
-	
-	if bin_weight < 80 #
-		bin_type = 'organic'
-		print('Bin Type: organic')
-	elif bin_weight >= 80 and bin_weight < 110
-		bin_type = 'ceramic'
-		print('Bin Type: ceramic')
-	else	
-		bin_type = 'metallic'
-		print('Bin Type: metallic')
-		
-	return bin_type
+###############################################################################
+## Notes
+###############################################################################
+#MotorA -> (+) - lift arm up
+#       -> (-) - lower arm
 
-#CHECK NUMBERS -> tacho distance, power
-def lift_bin(touch_sensor, motor_arm):
-	initial_pos = motor_arm.get_tacho()
-	max_distance = 15 #number to be changed
-	power = 50 #number to be changed
-	time = []
-	
-	while get_tacho() - initial_pos <= max_distance 
-		motor_arm.run(power = power)
-		if touch_sensor.is_pressed() == FALSE:
-			time.append(time())
-			
-	motor_arm.idle()
-	stop = time()
-	total_time = stop - time[0]
-	print(total_time)
-	
-	return total_time
+
+
+###############################################################################
+## Action
+###############################################################################
+
+#Lift bin and identify the bin type
+def lift_identify(motor_arm):
+    max_distance = 65
+    power1 = 64.95
+    power2 = 69
+    
+    initial_pos = int(motor_arm.get_tacho().block_tacho_count)   #based garrett
+    
+    while int(motor_arm.get_tacho().block_tacho_count) - initial_pos <= max_distance:
+        motor_arm.run(power = power1)
+        time.sleep(.8)
+        if int(motor_arm.get_tacho().block_tacho_count) - initial_pos < max_distance:
+            motor_arm.run(power = power2)
+            time.sleep(.3)
+            if int(motor_arm.get_tacho().block_tacho_count) - initial_pos <= max_distance:
+                motor_arm.brake()
+                bin_type = 'metallic'
+            else:
+                motor_arm.brake()
+                bin_type = 'ceramic'
+        else:
+            motor_arm.brake()
+            bin_type = 'organic'
+                
+    motor_arm.brake()
+    return(bin_type)
 	
 #TO DO	
-def travel_to(bin_type):
+'''def travel_to(bin_type):
 	if bin_type == 'organic':
 		#robot travels to organic bin drop off
-	
 	elif bin_type == 'ceramic'
 		#robot travels to ceramic bin drop off
-	
 	elif bin_type == 'metallic'
-		#robot travles to metallic bin drop off
+		#robot travles to metallic bin drop off'''
 	
