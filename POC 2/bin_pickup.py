095aba555ef5ff9888a60167827dbbb5d167d698
maokat12 <maokat12@gmail.com>
2016-11-15 22:08:00 -0500

    Re-did bin pickup/dropoff

--- POC 2/bin_pickup.py
+++ POC 2/bin_pickup.py
@@ -47,30 +47,37 @@ motorC = Motor(brick, PORT_C) #left
 ## Action
 ###############################################################################
 
-while True: 
-	dist = float(ultrasonic.get_distance())/256
-	print(dist)
-	
-	if dist < .06: #stop motors
-		motorB.run(power = 0)
-		motorC.run(power = 0)
+while True:
+    dist = float(ultrasonic.get_distance())/256
+    print(dist)
+    
+    if dist <= 0.029:
+        motorB.run(power = 0)
+        motorC.run(power = 0)
+        
+        print(lift_identify(motorA))
+        time.sleep(1)
+        
+        motorB.run(power = 80)
+        motorC.run(power = 80)
+        time.sleep(3)
+        
+        motorB.brake()
+        motorC.brake()
+        
+        motorA.run(-70)
+        time.sleep(1)
+        motorA.idle()
+        
+        motorB.run(-90)
+        motorC.run(-90)
+        time.sleep(5)
+        
+        motorB.brake()
+        motorC.brake()        
+        
+        break
 		
-		time = lift_bin(motorA, touch)  #pick up bin
-		sleep(10)  #wait
-		
-		motorB.run(power = 20)   #move a little bit
-		motorC.run(power = 20)
-		sleep(5)  #wait
-		
-		while(touch.is_pressed == FALSE)  #sets bin b
-			motorA.run(power = -20)
-		
-	elif dist >= 0.2 and dist < 0.3: #low speed
-		motorB.run(power = -65 - (5*dist))
-		motorC.run(power = -65 - (5*dist))
-	elif dist >= 0.06 and dist < 0.2: #medium speed
-		motorB.run(power = -70 - (10 * dist))
-		motorC.run(power = -70 - (10 * dist))
-	else: #high speed
-		motorC.run(power = -80 - (10 * dist))
-		motorB.run(power = -80 - (10 * dist))
+    elif dist > 0.029: #low speed
+		motorB.run(power = 80)
+		motorC.run(power = 80)
